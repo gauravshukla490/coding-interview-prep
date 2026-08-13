2f695760cbcf0ec3bf5316d49957305ae4a4cb1e
shukla gaurav <shuklaagaurav29@gmail.com>
2026-08-13 06:36:48 +0530

    detect cycle in directed graph using kahn's algorithm

---
+++ graph/directed_cycle_detection_using_topo_sort.cpp
@@ -0,0 +1,39 @@
+class Solution {
+  public:
+    bool isCyclic(int V, vector<vector<int>> &edges) {
+    //create adjacency list and inDegree array
+    vector<vector<int>>adj(V);
+    vector<int>inDegree(V,0);
+    for(int i=0 ; i<edges.size();i++){
+        int u = edges[i][0];
+        int v = edges[i][1];
+        
+        adj[u].push_back(v);
+        inDegree[v]++;
+    }
+        
+        queue<int>q;
+        vector<int>ans;
+        
+        for(int i=0 ; i<V ; i++){
+            if(inDegree[i] == 0){
+                q.push(i);
+            }
+        }
+        
+        while(!q.empty()){
+            int node = q.front();
+            q.pop();
+            
+            ans.push_back(node);
+            
+            for(auto it : adj[node]){
+                inDegree[it]--;
+                
+                if(inDegree[it]==0) q.push(it);
+            }
+        }
+        
+        return ans.size() == V ? false : true ;
+    }
+};
